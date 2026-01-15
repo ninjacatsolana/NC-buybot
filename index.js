67a692d6ec34df7ebc6d883819616398175cda3e
ninjacatsolana <ninjacatsolana@gmail.com>
2026-01-14 20:59:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -229,12 +229,14 @@ app.get("/overlay", (req, res) => {
 });
 
 
+
 // ---------- Start ----------
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
 
 
 
