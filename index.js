1588715b62abc101feb49f07f740e088069bdbf5
ninjacatsolana <ninjacatsolana@gmail.com>
2026-01-15 23:44:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+console.log("BOOTING NC BUYBOT", new Date().toISOString());
+
 const express = require("express");
 const { TwitterApi } = require("twitter-api-v2");
 require("dotenv").config();
@@ -332,6 +334,8 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
 
 
 
