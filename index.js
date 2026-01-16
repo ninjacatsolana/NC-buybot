6c66073cf04ce4b9e9ef2e35f1b4091ca4133c3a
ninjacatsolana <ninjacatsolana@gmail.com>
2026-01-15 23:31:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 
 const app = express();
 app.use(express.json({ limit: "2mb" }));
+app.get("/ping", (req, res) => res.status(200).send("ok"));
 
 // --- Buy Meter State ---
 let sessionUsd = 0;
@@ -331,6 +332,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
 
 
 
