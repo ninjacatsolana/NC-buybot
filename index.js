05f17ca52aae7a222e3532a19ed6833c14bc9bc3
ninjacatsolana <ninjacatsolana@gmail.com>
2026-01-19 20:39:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -204,7 +204,8 @@ app.post("/helius", async (req, res) => {
 });
 
 // 19 ---------- Start ----------
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
