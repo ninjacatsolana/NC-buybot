8225abfafd5e0ee83ecff1f1e431a3559fb153fc
ninjacatsolana <ninjacatsolana@gmail.com>
2026-02-16 14:12:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -46,9 +46,13 @@ async function tweetWithImage(text) {
 }
 
 // Health
-app.get("/health", (req, res) => {
-  res.status(200).send("ok");
+app.get("/version", (req, res) => {
+  res.json({
+    version: "buybot-v3-id",
+    time: Date.now(),
+  });
 });
+
 
 // Overlay URL for Streamlabs browser source
 app.get("/overlay", (req, res) => {
@@ -213,4 +217,5 @@ app.post("/helius", async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("listening on", PORT));
+
 
